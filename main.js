312e6a742bbd56a65698e84ce69db20ba77daf34
victoriaowolijah <victoriaowolijah@gmail.com>
2022-08-09 23:02:12 +0100

    changes

--- main.js
+++ main.js
@@ -83,3 +83,25 @@ var swiper = new Swiper(".featured-slider", {
       },
     },
   });
+
+
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 6500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
